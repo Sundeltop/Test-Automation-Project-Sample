1a97451cdc933ca5a8c7e25bcbfec2539b8c8fce
Oleksandr Nelipa <sundeltop322@gmail.com>
2022-06-06 22:24:09 +0300

    log4j2

--- src/test/java/com/sample/extensions/LoggerExtension.java
+++ src/test/java/com/sample/extensions/LoggerExtension.java
@@ -1,25 +1,25 @@
 package com.sample.extensions;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class LoggerExtension implements BeforeEachCallback, AfterEachCallback {
-    private final Logger logger = LogManager.getLogger(LoggerExtension.class);
+    private static final Logger logger = LogManager.getLogger(LoggerExtension.class);
 
     @Override
     public void beforeEach(ExtensionContext extensionContext) {
-        logger.info("Test %s is starting".formatted(extensionContext.getRequiredTestMethod().getName()));
+        logger.info("Test {} is starting", extensionContext.getRequiredTestMethod().getName());
     }
 
     @Override
     public void afterEach(ExtensionContext extensionContext) {
-        String logMessage = "Test %s is passed";
-        if (extensionContext.getExecutionException().isPresent()) {
-            logMessage = "Test %s is failed: " + extensionContext.getExecutionException().get().getCause().getMessage();
-        }
-        logger.info(logMessage.formatted(extensionContext.getRequiredTestMethod().getName()));
+        String methodName = extensionContext.getRequiredTestMethod().getName();
+        extensionContext.getExecutionException().ifPresentOrElse(
+                exception -> logger.warn("Test {} is failed {}",
+                        methodName, exception.getMessage()),
+                () -> logger.info("Test {} is passed", methodName));
     }
 }
